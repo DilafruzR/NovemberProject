69b4db83df3ba97f46360cda10815a063fef314d
DilafruzR <dilafruz81@gmail.com>
2020-11-01 11:45:17 -0500

    therd attempt

--- src/test/java/Flash.java
+++ src/test/java/Flash.java
@@ -1,6 +1,7 @@
 public class Flash {
     public static void main(String[] args) {
         System.out.println("Given I wikl try to learn fast");
+        System.out.println("Flash run really fast");
     }
 
 }
